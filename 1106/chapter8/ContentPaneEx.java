9af288371d916771cd0b5aa2468809e5af5a5c24
JMIN <134764937+JMY00n@users.noreply.github.com>
2025-11-07 09:52:06 +0900

    Add files via upload

---
+++ 1106/chapter8/ContentPaneEx.java
@@ -0,0 +1,32 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class ContentPaneEx extends JFrame {
+	private static final long serialVersionUID = 1L;
+
+	public ContentPaneEx() {
+		setTitle("ContentPane과 JFrame 예제");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Container contentPane = getContentPane();
+		contentPane.setBackground(Color.ORANGE);
+		contentPane.setLayout(new FlowLayout());
+		
+		contentPane.add(new JButton("OK"));
+		contentPane.add(new JButton("Cancle"));
+		contentPane.add(new JButton("Ignore"));
+		
+		setSize(300, 150);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new ContentPaneEx();
+	}
+}
